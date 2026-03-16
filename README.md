6fd5f567f9fb565d4b35cd25a61bd0e36772b80b
Poke13617 <cte-mason.curtis@sllboces.org>
2026-03-16 14:09:59 +0000

    fixed + made take_2 (holds original code, starting fresh to try and shrink the amount of code used.)

--- README.md
+++ README.md
@@ -4,16 +4,56 @@ What is this project?
 
 - This project is a text-based game where the goal will be to outlast a set timer. The NPCs will be 4 different allies that support you in the same sense as an asymmetrical horror game. The character chosen will have different kits as follows:
 
-  1.
+  1.Offense: Carries 2 in-your-face offense abilities that work very similar to each other. 
 
-  2.
+  2.Support: carries a heal that stays in place and a roll that carries a little mobility and a lower cooldown. Carries no stun time.
 
-  3.
+  3.Defense: carries a charge that damages itself and stuns the enemy for 4 turns, the highest stun time in the game at the tradeoff of no mobility
 
 
   Enemy:
 
-  1: 
+  1: The only enemy of the game, this enemy is intended to be 
 
 - All kits will have 2-3 abilities, ranging from a team heal to a long-duration stun. All NPCs that are used will play as shadows, providing support in areas that your kit needs (for example, support will result in your ally playing to stun while offense would result in your support being more for healing.
-- If the chance that all 4 allies lose to the enemy, your player character will be thrown in a Last Man Standing (or LMS for short) health will be set to 150 during LMS and certain abilities in the kit will be either stronger or have a better cooldown. timer will be set to 30 to keep time:
+- If the chance that your ally loses to the enemy, your player character will be thrown in a Last Man Standing (or LMS for short) health will be set to 150 during LMS, regardless of dmg taken. Certain abilities in the kit will be either stronger or have a better cooldown. timer will be set to 30 to keep time:
+
+
+If I have time at the end, I'd like to make a map-like area that would look like the following:
+(for reference: 0 is base level, 1 is a platform you can jump to, and 2 is a high-up platform you can only get to from level 1) (ways to get up)
+
+
+    0  0  1  2  2  2  2  2  0  0
+    0  2  2  2  0  0  1  2  1  0
+    0  2  1  0  0  2  2  2  2  0
+    1  2  2  0  0  1  0  0  2  0
+    0  2  0  0  0  0  0  0  0  0
+    0  2  1  0  0  0  0  1  2  0 
+    0  2  2  2  2  2  2  2  2  0
+    0  0  0  0  0  0  0  0  0  0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Balancing:
+
+Correlation: As the stun time increases, the intent is for the cooldown to be greater and the distance moving away from the enemy be less. The enemy has a damaging attack seeing 2 of the 3 kits carries 100 health and the defense kit can take 4-5 hits maximum. All kits will have 
+
+Here are the stats!
+
+
+
+Offense:
+atk-1: stun - 3 cd - 
+atk-2: stun - 2
--- file.py
+++ file.py
@@ -2,14 +2,17 @@ import random
 
 
 
+#Define variables, in particular the following: user/teammate dmg, cd, distance. Kit hp, stun counter, and the attacks with damage on enemy.
 
 
-def classes(): # This will be used to get the classes to use in-game (1998 is a dev character)
-    class_num = int(input("Enter a number, 1 for offensive, 2 for support, 3 for defensive/survivalist:"))
-    if class_num != 1 and class_num != 2 and class_num != 3 and != 1998:
-        print("Not a valid class:")
-        class_num = int(input("Enter a number, 1 for offensive, 2 for support, 3 for defensive/survivalist:"))
-    if class_num == 1:
+
+
+def classes(): # This will be used to get the classes to use in-game
+    class_num = int(input("Enter a number, 1 for offensive, 2 for support, 3 for defensive/survivalist:")) # Class selector
+    if class_num != 1 and class_num != 2 and class_num != 3:
+        print("Not a valid class:") # Confirms a class is seected
+        class_num = int(input("Enter a number, 1 for offensive, 2 for support, 3 for defensive/survivalist:")) # Restarts class selector
+    if class_num == 1: # Offense kit/Support ally
         class_name = "Offense"
         class_hp = 100
         defense_offense = 1
@@ -20,7 +23,7 @@ def classes(): # This will be used to get the classes to use in-game (1998 is a 
         defense_teammate = 1
         list_teammate = [2, teammate_name, teammate_hp, defense_teammate]
 
-    elif class_num == 2:
+    elif class_num == 2: # Support kit/Defense ally
         class_name = "Support"
         class_hp = 100
         defense_support = 1
@@ -31,7 +34,7 @@ def classes(): # This will be used to get the classes to use in-game (1998 is a 
         defense_teammate = .5
         list_teammate = [3, teammate_name, teammate_hp, defense_teammate]
 
-    elif class_num == 3:
+    elif class_num == 3: # Defense kit/Offense ally
         class_name = "Defense"
         class_hp = 150
         defense_defense = 1.5
@@ -62,5 +65,75 @@ elif difficulty_num == 3:
     damage_multi = 1.5
     num_turns = 50
 difficulty_stats = [difficulty, damage_multi, num_turns]
+
+
+# Little note I should add again here, damage and all damage-related information is used as stun time, which is why damage appears as lower.
+
+
+
+if class_name == "Offense":
+    attack_1 = "Dash"
+    attack_1_dmg = 2
+    attack_1_distance = 1
+    
+    attack_2 = "Roll"
+    attack_2_dmg = 1
+    attack_2_distance = 3
+    attack_2_cd = 5
+
+    teammate_1_1 = "Heal"
+    teammate_1_1_dmg = 0
+    teammate_1_1_distance = 4
+    teammate_1_1_cd = 3
+
+    teammate_1_2 = "dodge"
+    teammate_1_2_dmg = 0
+    teammate_1_2_distance = 2
+    teammate_1_2_cd = 2
+
+elif class_name == "Support":
+    attack_1 = "Heal"
+    attack_1_dmg = 0
+    attack_1_distance = 4
+    attack_1_cd = 3
+
+    attack_2 = "dodge"
+    attack_2_dmg = 0
+    attack_2_distance = 2
+    attack_2_cd = 2
+
+    teammate_1_1 = "Charge"
+    teammate_1_1_dmg = 4
+    teammate_1_1_distance = 2
+    teammate_1_1_cd = 2
+
+    teammate_1_2 = "Self-heal"
+    teammate_1_2_dmg = 0
+    teammate_1_2_distance = 4
+    teammate_1_2_cd = 3
+
+
+elif class_name == "Defense":
+    attack_1 = "Charge"
+    attack_1_dmg = 4
+    attack_1_distance = 2
+    attack_1_cd = 2
+
+    attack_2 = "Self-heal"
+    attack_2_dmg = 0
+    attack_2_distance = 4
+    attack_2_cd = 3
+
+    teammate_1_1 = "Dash"
+    teammate_1_1_dmg = 2
+    teammate_1_1_distance = 1
+    teammate_1_1_cd = 4
+
+    teammate_1_2 = "Roll"
+    teammate_1_2_dmg = 1
+    teammate_1_2_distance = 3
+    teammate_1_2_cd = 5
+
+
 for turns_passed in num_turns:
-    pass
+    
