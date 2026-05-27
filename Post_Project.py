2ab625346f34161f2459395b562ccdc681c386ec
Poke13617 <cte-mason.curtis@sllboces.org>
2026-05-27 12:35:49 +0000

    ...

--- Post_Project.py
+++
@@ -1,7 +0,0 @@
-# My section of the game we made with the last few weeks of the school year: "The Life of Elijah"
-
-# Boss's theme: Games
-
-' What the boss could contain:'
-' Survival fight: This boss will not contain relavency to any items you have, instead focusing on surviving in a different setting. I plan to make it similar to my senior project because my senior project was intended to take my game and make it into an asymmetrical horror survival. This would be a good idea because I could take that opportunity to make it 5 layers in elevation, containing summoned enemies that occasionally show up to do the work, a 100 second survival timer, moving obstacles meant to stop you, and even projectiles that will come from portions of the screen at random (with a timer of 2 turns before they go off of course.)'
-' If applicable, the boss will either be a double-feature (Taking zander, a junior) with a second character who will be there as the big enemy that stays on the map to chase and stop you in place. He also carries a single damaging attack that will not do much, but will stun. The projectiles from the boss up top will '
